175cbdfd80ad87c7c6772a47f3fd7a3474eb4dec
Hunterpp <hunter.peress@kaltura.com>
2021-08-29 08:58:14 -0400

    pkg updates

--- src/components/uiComponents/property/index.tsx
+++ src/components/uiComponents/property/index.tsx
@@ -9,6 +9,7 @@ interface Props {
   value?: number | string;
   label: string;
   required: boolean;
+  description: string;
   props: any;
   onChange(id: any, value: any): void;
 }
@@ -19,14 +20,17 @@ class Property extends Component<Props> {
     onChange(id, value);
   };
   render() {
-    const { value, label, required, props } = this.props;
+    const { value, label, required, props, description } = this.props;
     const PropertyComponent: any = getPropertyComponent(this.props.type);
     if (!PropertyComponent) {
       return null;
     }
     return (
       <div className="property">
-        <span className="property-label">{label}</span>
+        <span className="property-label">{label}<a className="">
+          <i className="">j</i>
+        </a></span>
+        
         <PropertyComponent
           value={value}
           onChange={this.handleOnChange}
